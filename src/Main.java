ec33f42151aaa9d473ae2ea38be69a43de3776dd
Ranbeer Singh <ranbeersinghcauhan@gmail.com>
2025-02-28 09:12:25 +0530

    first commit

---
+++ src/Main.java
@@ -0,0 +1,64 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+import java.util.Random;
+
+public class Main {
+    private static final int BOARD_SIZE = 100;
+    private static final int[] SNAKES = {16, 47, 49, 56, 62, 64, 87, 93, 95, 98};
+    private static final int[] SNAKE_ENDS = {6, 26, 11, 53, 19, 60, 24, 73, 75, 78};
+    private static final int[] LADDERS = {1, 4, 9, 21, 28, 36, 51, 71, 80};
+    private static final int[] LADDER_TOPS = {38, 14, 31, 42, 84, 44, 67, 91, 100};
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        int[] playerPositions = {0, 0};
+        String[] playerNames = {"You", "Opponent"};
+        int currentPlayer = 0;
+
+        while (true) {
+            System.out.println("\n" + playerNames[currentPlayer] + "'s turn");
+            System.out.println("Press Enter");
+            scanner.nextLine();
+
+            int diceRoll = random.nextInt(6) + 1;
+            System.out.println("Your number " + diceRoll);
+
+            int newPosition = playerPositions[currentPlayer] + diceRoll;
+
+            for (int i = 0; i < SNAKES.length; i++) {
+                if (newPosition == SNAKES[i]) {
+                    System.out.println("Moving down, you get Snake bite.  " + SNAKE_ENDS[i]);
+                    newPosition = SNAKE_ENDS[i];
+                    break;
+                }
+            }
+
+            for (int i = 0; i < LADDERS.length; i++) {
+                if (newPosition == LADDERS[i]) {
+                    System.out.println("Moving up to " + LADDER_TOPS[i]);
+                    newPosition = LADDER_TOPS[i];
+                    break;
+                }
+            }
+
+            if (newPosition > BOARD_SIZE) {
+                newPosition = playerPositions[currentPlayer];
+            }
+
+            playerPositions[currentPlayer] = newPosition;
+            System.out.println(playerNames[currentPlayer] + " is at position " + newPosition + " now");
+
+            if (newPosition == BOARD_SIZE) {
+                System.out.println(playerNames[currentPlayer] + " wins!");
+                break;
+            }
+
+            currentPlayer = 1 - currentPlayer;
+        }
+
+        scanner.close();
+    }
+}
